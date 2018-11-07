ce663fb05fae7e994dfeafe3a07f16d250cf68ff
StevenZiga <sjzfranco@gmail.com>
2018-11-07 16:49:11 -0600

    Tercer update

--- php/login.php
+++ php/login.php
@@ -1,12 +1,33 @@
 <?php 
-    /////Conexión con la data//////////
-    $hostname = "sql203.epizy.com";
-    $database = "epiz_22952208_BD_BolsaEmpleo";
-    $username = "epiz_22952208";
-    $password = "progra";
-    $con = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR); 
-    
-    if(isset($_POST['save'])){
+  /////Conexión con la data//////////
+  $hostname = "sql203.epizy.com";
+  $database = "epiz_22952208_BD_BolsaEmpleo";
+  $username = "epiz_22952208";
+  $password = "progra";
+  $con = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);  
+
+  if(isset($_POST['acceder'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($con,$_POST['username']); 
+    $password = mysqli_real_escape_string($con,$_POST['password']); 
+
+    mysql_select_db($database, $con);
+    $sql = "SELECT username FROM users WHERE username = '$username' and pass = '$password'";
+    $result = mysql_query($con,$sql);
+
+    $row = mysql_fetch_array($result);
+
+    if($row['username']==$username && $row['pass']==$password){
+      echo "<p>BIENVENIDO ".$row['username']."!</p>";
+    }else{
+      echo "ERROR AL INICIAR SESION!";
+    }
+  }
+?>
+
+<!--     if(isset($_POST['save'])){
       $user = $_POST['username'];
       $pass = $_POST['password'];
       mysql_select_db($database, $con);
@@ -18,4 +39,4 @@
 
       echo "<SCRIPT> alert('$message'); </SCRIPT>";
     }
-  ?>
+  ?> -->
--- registro_empresa.php
+++ registro_empresa.php
@@ -24,6 +24,7 @@
 
     <!-- Custom styles for this template -->
     <link href="css/estilo.min.css" rel="stylesheet">
+    <link rel="icon" href="img/logos/logo-cuc-title.ico">
 
   </head>
 
