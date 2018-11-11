ebeac3c667eec6ed4cdb7607134ccf76d6ff3d2a
Brayanbren <bryan.brenes.pere@gmail.com>
2018-11-10 21:24:07 -0600

    Oferente.php con cédula del oferente
    -Se agrega validación al campo de la cédula.
    -Insert con cédula.

--- php/oferente.php
+++ php/oferente.php
@@ -8,6 +8,7 @@
     
     if(isset($_POST['register'])){
       $fullname = $_POST['fullname'];
+      $cedofe = $_POST['idoferente'];
       $password = $_POST['password'];
       $email = $_POST['email'];
       $phonenumber = $_POST['phonenumber'];
@@ -28,9 +29,9 @@
       $discapacidad = $_POST['discapacidad'];
       $curriculum = $_POST['curriculum'];
 
-      if($fullname!="" && $password!="" && $email!="" && $phonenumber!="" && $birthdate!="" && $nationality!="" && $age!="" && $placelive!="" && $genero!="" && $studies!="" && $estudia!="" && $languages!="" && $herramienta!="" && $nivelcono!="" && $licencia!="" && $vehiculo!="" && $horario!="" && $earnings!="" && $discapacidad!="" && $curriculum!=""){
+      if($fullname!="" && $cedofe!="" && $password!="" && $email!="" && $phonenumber!="" && $birthdate!="" && $nationality!="" && $age!="" && $placelive!="" && $genero!="" && $studies!="" && $estudia!="" && $languages!="" && $herramienta!="" && $nivelcono!="" && $licencia!="" && $vehiculo!="" && $horario!="" && $earnings!="" && $discapacidad!="" && $curriculum!=""){
         mysql_select_db($database, $con);
-        $sql = "INSERT INTO RegistroOferente (fullname, password, email, phonenumber, birthdate, nationality, age, placelive, genero, studies, estudia, languages, herramienta, nivelcono, licencia, vehiculo, horario, earnings,discapacidad, curriculum) VALUES ('$fullname','$password', '$email','$phonenumber', '$birthdate','$nationality', '$age','$placelive', '$genero','$studies', '$estudia','$languages', '$herramienta','$nivelcono', '$licencia','$vehiculo', '$horario','$earnings','$discapacidad','$curriculum')";
+        $sql = "INSERT INTO RegistroOferente (fullname,cedula ,password, email, phonenumber, birthdate, nationality, age, placelive, genero, studies, estudia, languages, herramienta, nivelcono, licencia, vehiculo, horario, earnings,discapacidad, curriculum) VALUES ('$fullname','$cedofe' ,'$password', '$email','$phonenumber', '$birthdate','$nationality', '$age','$placelive', '$genero','$studies', '$estudia','$languages', '$herramienta','$nivelcono', '$licencia','$vehiculo', '$horario','$earnings','$discapacidad','$curriculum')";
         $sqluser = "INSERT INTO users(username, pass) VALUES ('$email','$password')";
         $rspubli = mysql_query($sql, $con) or die(mysql_error());
         $result = mysqli_query($con,$sql);
@@ -39,11 +40,11 @@
         $resultuser = mysqli_query($con,$sqluser);  
 
         echo "<div class='alert alert-success'>
-                <strong>Registro completado!</strong><p>Su información ha sido guardada exitosamente!</p>
+                <strong>¡Registro completado!</strong><p>Su información ha sido guardada exitosamente.</p>
               </div>";
       }else{
         echo "<div class='alert alert-danger'>
-                <strong>Error en el registro!</strong> <p>Verifique que todos los campos estén completados!</p>
+                <strong>¡Error en el registro!</strong> <p>Verifique que todos los campos estén completados.</p>
               </div>";
       }
       }
